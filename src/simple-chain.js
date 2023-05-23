7065b9ff320efd15d9b7b92f8ac432e111281fce
Neiman22 <pushkarev_av@inbox.ru>
2023-05-23 09:47:37 +0300

    simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,26 +4,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
+ const chainMaker = {
+  arrayChain: [],
+
   getLength() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    this.arrayChain.push(value);
   },
+
   removeLink(/* position */) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
+
   reverseChain() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let resultArray = [];
+    for (let i = 0; i < this.arrayChain.length; i++) {
+      resultArray.push(`( ${this.arrayChain[i]} )`);
+    }
+    return resultArray.join('~~');
   }
 };
 
