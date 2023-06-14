cd49c05de972729c17480a6d916f51094bf16f2f
Neiman22 <pushkarev_av@inbox.ru>
2023-06-14 10:10:33 +0300

    vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,68 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error ('Incorrect arguments!')
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    
+    let resultMessage = '';
+    let keyIndex = 0;
+    const regex = /[A-Z]/g;
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].match(regex)) {
+        const currSymbol = message[i].codePointAt() - 65 ;
+        const shift = key[keyIndex % key.length].codePointAt() - 65;
+        const newCurrSymbol = (currSymbol + shift) % 26;
+        resultMessage += String.fromCodePoint(newCurrSymbol + 65);
+        keyIndex++;
+      } else {
+        resultMessage += message[i];
+      }
+    }
+
+    if (this.direct) {
+      return resultMessage;
+    } else {
+      return resultMessage.split('').reverse().join('');
+    }
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error ('Incorrect arguments!')
+    }
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let resultMessage = '';
+    let keyIndex = 0;
+    const regex = /[A-Z]/g;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (encryptedMessage[i].match(regex)) {
+        const currSymbol = encryptedMessage[i].codePointAt() - 65;
+        const shift = key[keyIndex % key.length].codePointAt() - 65;
+        const newCurrSymbol = (currSymbol - shift + 26) % 26;
+        resultMessage += String.fromCodePoint(newCurrSymbol + 65);
+        keyIndex++;
+      } else {
+        resultMessage += encryptedMessage[i];
+      }
+    }
+
+    if (this.direct) {
+      return resultMessage;
+    } else {
+      return resultMessage.split('').reverse().join('');
+    }
   }
 }
 
